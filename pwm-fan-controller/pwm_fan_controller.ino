bd2741f8e08d3639d2a3c5ecf2b4629b819fab9f
maarten de klerk <maarten@deklerk.dev>
2020-07-30 19:07:25 +0200

    Added smoothing to potentiometer input
    As I forgot it on the hardware.

--- pwm-fan-controller/pwm_fan_controller.ino
+++ pwm-fan-controller/pwm_fan_controller.ino
@@ -6,15 +6,29 @@
 
 #define pwm_fan_pin 3
 #define potentiometer_pin A0
+
 #define potentiometer_steps 20
+#define readings_size 15
+
+/* Values for smoothing */
+int readings[readings_size];
+int read_index;
+int total;
+int average;
+
+int potentiometer_step;
 
 Adafruit_SH1106 display(-1);
 
 void setup()
 {
   pinMode(pwm_fan_pin, OUTPUT);
+
+  /* Set the values of the array to 0 */
+  memset(readings, 0, readings_size);
+
+  /* Initialize the display */
   display.begin(SH1106_SWITCHCAPVCC, 0x3C);
-  
   display.display();
   delay(2000);
   display.clearDisplay();
@@ -25,14 +39,44 @@ void setup()
 
 void loop()
 {
-  const unsigned int temp = analogRead(potentiometer_pin);
-  const unsigned int potentiometer_step = map(temp, 0, 1000, 0, potentiometer_steps);
+  potentiometer_step = read_potentiometer();
   analogWrite(pwm_fan_pin, (255 / potentiometer_steps) * potentiometer_step);
 
-  display_info(pwm_fan_duty_cycle * 5);
+  display_info(potentiometer_step * 5);
+  
+  delay(1);
 }
 
-void display_info(const unsigned int duty_cycle_percentage)
+/**
+ * Read the potentiometer and smooth the output.
+ * 
+ * @returns the smoothed output as int.
+ */
+int read_potentiometer()
+{
+  total -= readings[read_index];
+  readings[read_index] = analogRead(potentiometer_pin);
+  total += readings[read_index];
+
+  read_index += 1;
+  if (read_index >= readings_size) read_index = 0;
+ 
+  average = total / readings_size;
+  /* analogRead ranges from 0 to 1024, but it rarly reaches the max value. 
+   * In order to get a 100% reading on the display it is changed to 1000.
+   */
+  average = map(average, 0, 1000, 0, potentiometer_steps);
+  average = constrain(average, 0, potentiometer_steps);
+
+  return average;
+}
+
+/**
+ * Draws the potentiometer value on SH1106 128x64 oled screen.
+ * 
+ * @param duty_cycle_percentage The number to display on the screen.
+ */
+void display_info(const int duty_cycle_percentage)
 {
   display.clearDisplay();
 
